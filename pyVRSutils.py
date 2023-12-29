72e7d930507b1d7c4aa455f6597ccb746de4ad53
seastate <danny2468@gmail.com>
2023-12-29 10:43:37 -0800

    Add type(ndarray) to n2s_fmt

--- pyVRSutils.py
+++ pyVRSutils.py
@@ -1,12 +1,12 @@
 #
 #   Shared utilities and functions for the pyVRS hydrodynamics codes
 #
-
+from numpy import ndarray
 #==============================================================================
 # A utility to format numbers or lists of numbers for graphical output
 def n2s_fmt(f,fmt='7.3e'):
     _fmt = '{:'+fmt+'}'
     if type(f)==int or type(f)==float:
         return _fmt.format(f)
-    if type(f)==list:
+    if type(f)==list or type(f)==ndarray:
         return [_fmt.format(_f) for _f in f]
