ab3e07a08b42da54ff5b281735b3044b401a8d8e
dg <danny2468@gmail.com>
2024-05-18 12:45:53 -0700

    Add examples

---
+++ Examples/Example1.py
@@ -0,0 +1,140 @@
+"""
+This code demonstrates creating model of an early embryo using two 
+constitutive chimeras representing the exterior surface and an inclusion.
+It shows the basic usage of codes calculating surface and body forces on 
+the embryo, and its swimming in a specified flowfield. It also demonstrates
+scaling properties, testing whether the dimensional analysis correctly 
+identifies different scenarios that are scales models of each other.
+
+"""
+
+# import required modules
+from copy import deepcopy
+import numpy as np
+from math import pi
+from MinimalAttrDict import AttrDict
+from pprint import pprint as pprnt
+
+from matplotlib import pyplot as plt
+plt.ion()
+
+from pyVRSchimera import chimeraParams, shape_scaleParams, chimeraMorphology, print_cp
+from pyVRSchimera import ScaleParams, ShapeParams, MeshParams
+from pyVRSflow import SimPars, VRSsim
+from pyVRSdata import Materials, get_MatlParams, MorphMatrix, load_morph, SimMatrix, DataManager
+
+#====================================================================
+# Scaling test, Step 1: Create an example dimensional parameter set
+# Because we don't know the shape/scaling parameters, create a dummy set
+# of parameters, modify them to reflect the example, then get shape &
+# scale parameters.
+#====================================================================
+# create parameter AttrDicts, using default parameters
+scaleParams = ScaleParams()
+shapeParams = ShapeParams()
+meshParams = MeshParams()
+# Get material parameters (by default, scaled by Delta-rho=1, so
+# unless this is changed the numerical values for density do not change).
+# However the viscosity is normalized by the viscosity of the reference
+# material (by default, seawater).
+MatlParams = get_MatlParams()
+# Extract basic properties, in the order of Layers (Layer 0: medium;
+# Layer 1: tissue; Layer 2: inclusion).
+excess_densities = [MatlParams['seawater'].density,
+                    MatlParams['tissue'].density,
+                    MatlParams['freshwater'].density]
+colors_ = [MatlParams['seawater'].color,
+           MatlParams['tissue'].color,
+           MatlParams['freshwater'].color]
+
+# calculate the corresponding chimera template, with the default parameter set
+cp = chimeraParams(shape_pars=shapeParams,scale_pars=scaleParams,
+                  mesh_pars=meshParams,densities=excess_densities,
+                  colors=colors_)
+# cp is now the template (default) chimera parameter set; print values
+# using the print_cp utility:
+print_cp(cp)
+
+# Modify the chimeraParams object with some reasonable dimensional parameters
+# medium properties
+cp.Layers[0].mu = 1030.*1.17e-6
+#cp.Layers[0].density = 1030.
+# Surface properties
+#cp.Layers[1].V_s
+cp.Layers[1].D = 150.e-6
+cp.Layers[1].L1 = 100.e-6
+cp.Layers[1].L2 = 60.e-6
+cp.Layers[1].L0 = cp.Layers[1].L1 +  cp.Layers[1].L2
+#cp.Layers[1].density = Materials.tissue.density
+#cp.Layers[1].d = 6.e-6
+#cp.Layers[1].nd = 12 #16
+#cp.Layers[1].nlevels = (16,16)
+cp.Layers[1].translate = None
+
+#Inclusion properties
+#cp.Layers[2].V_i
+cp.Layers[2].D = 80.e-6
+cp.Layers[2].L1 = 60.e-6
+cp.Layers[2].L2 = 50.e-6
+cp.Layers[2].L0 = cp.Layers[2].L1 +  cp.Layers[2].L2
+cp.Layers[2].h_i = 30.e-6
+#cp.Layers[2].d = 5.e-6
+#cp.Layers[2].nd = 12 #16
+#cp.Layers[2].nlevels = (16,16)
+#cp.Layers[2].density = Materials.freshwater.density
+cp.Layers[2].translate = [0.,0.,cp.Layers[2].h_i]
+# print the modified chimera parameters, reflecting a plausible example
+print_cp(cp)
+
+#============================================================================
+# Calculate the corresponding shape/scale params; print values to confirm
+sh,scD,msh,dens,clrs = shape_scaleParams(cp)
+print('Dimensional parameters:')
+print(dict(sh))
+print(dict(scD))
+print(dens)
+print(dict(msh))
+print(clrs)
+# calculate chimera parameters corresponding to the dimensional characteristics
+cpD = chimeraParams(shape_pars=sh,scale_pars=scD,
+                  mesh_pars=msh,densities=dens,
+                  colors=clrs)
+# print to compare to cp:
+print_cp(cpD)
+# record dimensional length and time scales
+l_D = cpD.l
+tauD = cpD.tau
+
+# generate the dimensional chimera morphology
+cM = chimeraMorphology(chimera_params=cpD,plotMorph=False,calcFlow=True,calcBody=True)
+# visualize the surface and tiling
+figureM = plt.figure(num=27)
+figureM.clf()
+axesM = figureM.add_subplot(projection='3d')
+cM.plot_layers(axes=axesM,showFaces=True,showEdges=False)
+
+figureM2 = plt.figure(num=37)
+figureM2.clf()
+axesM2 = figureM2.add_subplot(projection='3d')
+cM.plot_layers(axes=axesM2,showFaces=False,showEdges=True,autoscale=True)
+
+
+#===========================================
+# create a set of simulation parameters
+sp = SimPars()
+#sp.Tmax = 3.
+sp.Tmax = 50.
+sp.cil_speed = 500.e-6
+sp.dt = 0.1
+sp.dudz = -0.25
+sp.dwdx = 0.25
+#sp.plotSim = 'all'
+sp.plotSim = 'end'
+#sp.plotSim = 'intvl'
+sp.plot_intvl = 50
+sp.print()
+# create a simulation object
+vs = VRSsim(morph=cM,simPars=sp,fignum=47)
+# run the simulation
+vs.runSP(simPars=sp)
+
---
+++ Examples/Example2.py
@@ -0,0 +1,102 @@
+"""
+This code demonstrates scaling properties, testing whether the dimensional analysis
+ correctly identifies different scenarios that are scales models of each other.
+"""
+# Import libraries, classes etc. from Example1
+from Examples.Example1 import *
+
+#============================================================================
+# test that scaling/nondimensionalization works as it should, by calculating
+# and simulating the nondimensional version of the scenario above.
+scND = ScaleParams(V_t=1.,mu=1.,Delta_rho=1.,g=1.)  # the nondimensional (default) scaling parameters
+# create a corresponding nondimensional version of cp; parameters are the same, except scaling parameters
+cpND = chimeraParams(shape_pars=sh,scale_pars=scND,
+                  mesh_pars=msh,densities=dens,
+                  colors=clrs)
+print_cp(cpND)
+cMND = chimeraMorphology(chimera_params=cpND,plotMorph=False,calcFlow=True,calcBody=True)
+
+figureMND = plt.figure(num=28)
+figureMND.clf()
+axesMND = figureMND.add_subplot(projection='3d')
+cMND.plot_layers(axes=axesMND,showFaces=True,showEdges=False)
+
+figureMND2 = plt.figure(num=38)
+figureMND2.clf()
+axesMND2 = figureMND2.add_subplot(projection='3d')
+cMND.plot_layers(axes=axesMND2,showFaces=False,showEdges=True,autoscale=True)
+
+#===========================================
+# create a set of simulation parameters
+spND = deepcopy(sp)
+# convert dimensional simulation parameters to nondimensional simulation parameters,
+# using the dimensional length and time scale
+spND.toND(l=l_D,tau=tauD)
+#spND.plotSim = 'all'
+#spND.dt = 0.01
+spND.print()
+# create a simulation object
+vsND = VRSsim(morph=cMND,simPars=spND,fignum=48)
+# run the simulation
+vsND.runSP(simPars=spND)
+
+
+
+#===========================================
+# graph some result comparisons
+skip_stat = 0
+# dimensional results
+timeD = np.asarray(vs.time)[skip_stat:]
+velocityD = np.asarray(vs.velocity)[skip_stat:,:]
+positionD = np.asarray(vs.position)[skip_stat:,:]
+extflowD = np.asarray(vs.extflow)[skip_stat:,:]
+# dimensional results
+timeND = np.asarray(vsND.time)[skip_stat:]
+velocityND = np.asarray(vsND.velocity)[skip_stat:,:]
+positionND = np.asarray(vsND.position)[skip_stat:,:]
+extflowND = np.asarray(vsND.extflow)[skip_stat:,:]
+
+# Dimensional vs. rescaled nondimensional results
+figureR = plt.figure(num=8)
+figureR.clf()
+axesR1 = figureR.add_subplot(1,2,1,projection='3d')
+# plot dimensional positions
+axesR1.plot(positionD[:,0],positionD[:,1],positionD[:,2],'r')
+# plot rescaled nondimensional positions
+axesR1.plot(l_D*positionND[:,0],l_D*positionND[:,1],l_D*positionND[:,2],'b-.')
+axesR1.set_xlabel('X')
+axesR1.set_ylabel('Y')
+axesR1.set_zlabel('Z')
+axesR2 = figureR.add_subplot(1,2,2,projection='3d')
+# plot dimensional velocitiestau
+axesR2.plot(velocityD[:,0],velocityD[:,1],velocityD[:,2],'r')
+# plot rescaled nondimensional velocities
+axesR2.plot(l_D/tauD*velocityND[:,0],l_D/tauD*velocityND[:,1],l_D/tauD*velocityND[:,2],'b-.')
+axesR2.set_xlabel('U')
+axesR2.set_ylabel('V')
+axesR2.set_zlabel('W')
+
+
+# Nondimensional vs. rescaled dimensional results
+figureS = plt.figure(num=9)
+figureS.clf()
+axesS1 = figureS.add_subplot(1,2,1,projection='3d')
+# plot nondimensional positions
+axesS1.plot(positionND[:,0],positionND[:,1],positionND[:,2],'b')
+# plot rescaled nondimensional positions
+axesS1.plot(1/l_D*positionD[:,0],1/l_D*positionD[:,1],1/l_D*positionD[:,2],'r-.')
+axesS1.set_xlabel('X')
+axesS1.set_ylabel('Y')
+axesS1.set_zlabel('Z')
+
+axesS2 = figureS.add_subplot(1,2,2,projection='3d')
+# plot dimensional velocities
+axesS2.plot(velocityND[:,0],velocityND[:,1],velocityND[:,2],'b')
+# plot rescaled nondimensional velocities
+axesS2.plot(tauD/l_D*velocityD[:,0],tauD/l_D*velocityD[:,1],tauD/l_D*velocityD[:,2],'r-.')
+axesS2.set_xlabel('U')
+axesS2.set_ylabel('V')
+axesS2.set_zlabel('W')
+
+
+
